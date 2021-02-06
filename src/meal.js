8214f210c11ced9eb4fb5ecbadcd7f0428349d4f
Plum <plummer.gv@gmail.com>
2021-02-05 21:15:18 -0700

    Add functions nameMenuItem, createMenuItem, addIngredients, formatPrice to meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,14 +1,42 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+  return `Delicious ${food}`
+};
 
+function createMenuItem(name, price, type){
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  };
+  return menuItem
+};
+
+function addIngredients(flavor1, flavor2){
+  if (flavor2.includes(flavor1)){
+    return console.log('${flavor1} is already part of this array.')
+  } else {
+  var moreIngredients = flavor2.push(flavor1);
+  return moreIngredients;
 }
+};
+
+function formatPrice(price) {
+  initialPrice = `$${price}`
+  return initialPrice
+};
+
+// function decreasePrice() {
+//   decreasedPrice = `(${menuItem.price} *.9)`;
+//   return decreasedPrice;
+// };
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
+   createMenuItem,
+   addIngredients,
+   formatPrice,
+   //decreasePrice,
   // createRecipe
 }
 
