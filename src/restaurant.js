0a16c7cf0cf2b34e3e3a1f8dde29e454f96ff453
Plum <plummer.gv@gmail.com>
2021-02-07 19:02:20 -0700

    add functions createRestaurant and addMenuItem to restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,32 @@
 
+function createRestaurant(name) {
+  var restaurant = {
+    name: name,
+    menus: menus = {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+  return restaurant;
+};
+
+function addMenuItem(restaurant, item) {
+  if (item.type === "breakfast" && !restaurant.menus.breakfast.includes(item)){
+    restaurant.menus.breakfast.push(item);
+  } else if (item.type === "lunch" && !restaurant.menus.breakfast.includes(item)){
+    restaurant.menus.lunch.push(item); 
+  } else if (item.type === "dinner" && !restaurant.menus.breakfast.includes(item)){
+    restaurant.menus.dinner.push(item);
+  } else {
+
+  };
+  };
+
+
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+   createRestaurant, 
+   addMenuItem,
   // removeMenuItem
 }
